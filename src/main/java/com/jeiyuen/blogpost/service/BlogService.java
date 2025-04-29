cf4f15722b6a4e405cc2a6f20440d1d96268f052
gjuanillo <gjuanillo.dev@gmail.com>
2025-04-29 16:34:21 +0800

    Implement Mapper to Service

--- src/main/java/com/jeiyuen/blogpost/service/BlogService.java
+++ src/main/java/com/jeiyuen/blogpost/service/BlogService.java
@@ -1,21 +1,21 @@
 package com.jeiyuen.blogpost.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-import com.jeiyuen.blogpost.entity.Blogs;
+import com.jeiyuen.blogpost.dto.BlogDTO;
+import com.jeiyuen.blogpost.dto.BlogUpdateDTO;
 
 public interface BlogService{
 
     //Find Blogs excluding details
-    List<Blogs> findall();
+    List<BlogDTO> findall();
     //Find Blog by ID, including details
-    Optional<Blogs> findBlogById(UUID id);
+    BlogDTO findBlogById(UUID id);
     //Save Blog on creation
-    Blogs saveBlog(Blogs blog);
+    BlogDTO saveBlog(BlogDTO dto);
     //Update blog
-    Blogs updateBlog(UUID id, Blogs blog);
+    BlogDTO updateBlog(UUID id, BlogUpdateDTO dto);
     //Delete blog
     void deleteBlog(UUID id);
     //TODO Handle media upload
--- src/main/java/com/jeiyuen/blogpost/service/BlogServiceImpl.java
+++ src/main/java/com/jeiyuen/blogpost/service/BlogServiceImpl.java
@@ -2,90 +2,66 @@ package com.jeiyuen.blogpost.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 
+import com.jeiyuen.blogpost.dto.BlogDTO;
+import com.jeiyuen.blogpost.dto.BlogUpdateDTO;
 import com.jeiyuen.blogpost.entity.Blogs;
+import com.jeiyuen.blogpost.mapper.BlogMapper;
 import com.jeiyuen.blogpost.repository.BlogRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class BlogServiceImpl implements BlogService{
 
     private final BlogRepository blogRepository;
+    private final BlogMapper blogMapper;
 
-    @Autowired
-    public BlogServiceImpl(BlogRepository theBlogRepository){
-        blogRepository = theBlogRepository;
+    public BlogServiceImpl(BlogRepository blogRepository, BlogMapper blogMapper) {
+        this.blogRepository = blogRepository;
+        this.blogMapper = blogMapper;
     }
 
     @Override
-    public List<Blogs> findall() {
-        return blogRepository.findAllByOrderByUpdatedDesc();
+    public List<BlogDTO> findall() {
+        return blogRepository.findAllByOrderByUpdatedDesc()
+            .stream().map(blogMapper::toDto).toList();
     }
 
     @Override
-    public Optional<Blogs> findBlogById(UUID id) {
-        // Optional<Blogs> blog = blogRepository.findById(id);
-        // Blogs theBlog = null; //Declare a temporary variable for a specific blog
-        // //Check if the ID exists within the database
-        // if(blog.isPresent()){
-        //     theBlog = blog.get();
-        // } else {
-        //     throw new RuntimeException("Blog not found with ID: " + id);
-        // }
-        // return theBlog;
-        return blogRepository.findById(id);
+    public BlogDTO findBlogById(UUID id) {
+        return blogRepository.findById(id).map(blogMapper::toDto)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Blog not found"));
     }
 
     @Override
-    public Blogs saveBlog(Blogs blog) {
-        if(blog.getUuid() != null){
-            throw new IllegalArgumentException("Id already exists");
-        }
-        if(blog.getTitle() == null){
-            throw new IllegalArgumentException("Title cannot be empty");
-        }
-        LocalDateTime now = LocalDateTime.now();
-        return blogRepository.save(new Blogs(
-                    null,
-                    blog.getTitle(),
-                    blog.getAuthor(),
-                    now,
-                    now));
+    public BlogDTO saveBlog(BlogDTO dto) {
+        Blogs blog = blogMapper.toEntity(dto);
+        blog.setCreationDate(LocalDateTime.now());
+        blog.setUpdated(LocalDateTime.now());
+        Blogs saved = blogRepository.save(blog);
+        return blogMapper.toDto(saved);
     }
-    
-    @Transactional
+
     @Override
-    public Blogs updateBlog(UUID id, Blogs blog) {
-        //Verify Request
-        if(blog.getTitle() == null){ //verify if title is empty
-            throw new IllegalArgumentException("Title cannot be empty");
-        }
-        if(blog.getAuthor() == null){ //verify if author is empty
-            throw new IllegalArgumentException("Author cannot be empty");
-        }
-        if (!Objects.equals(blog.getUuid(), id) && blog.getUuid() != null) { //verify if there's an attempt to change UUID
-            throw new IllegalArgumentException("Blog ID cannot be changed!");
-        }
-        Blogs existingBlog = blogRepository
-                .findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Blog ID does not exist!"));
-        existingBlog.setTitle(blog.getTitle());
-        existingBlog.setAuthor(blog.getAuthor());
-        existingBlog.setUpdated(LocalDateTime.now());
-        return blogRepository.save(existingBlog);
+    public BlogDTO updateBlog(UUID id, BlogUpdateDTO dto) {
+        Blogs blog = blogRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Blog with ID " + id + " does not exists!"));
+        blogMapper.updateEntity(blog, dto);
+        Blogs updated = blogRepository.save(blog);
+        return blogMapper.toDto(updated);
+
     }
-
-
 
     @Override
     public void deleteBlog(UUID id) {
-        blogRepository.deleteById(id);
+        Blogs blog = blogRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Blog with ID " + id + " does not exists!"));
+        blogRepository.delete(blog);
     }
+
 
 }
