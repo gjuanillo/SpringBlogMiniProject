8f31f8adf94fb066218e35926ff3762b10f5e7ce
gjuanillo <gjuanillo.dev@gmail.com>
2025-04-29 16:56:20 +0800

    Update Controller

--- src/main/java/com/jeiyuen/blogpost/controller/BlogController.java
+++ src/main/java/com/jeiyuen/blogpost/controller/BlogController.java
@@ -1,12 +1,14 @@
 package com.jeiyuen.blogpost.controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-import com.jeiyuen.blogpost.entity.Blogs;
+import com.jeiyuen.blogpost.dto.BlogDTO;
+import com.jeiyuen.blogpost.dto.BlogUpdateDTO;
 import com.jeiyuen.blogpost.service.BlogService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,57 +19,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class BlogController{
-    
-    //Inject Blog Service
+@RequestMapping("/api/blogs")
+public class BlogController {
+
+    // Inject Blog Service
     private final BlogService blogService;
 
-    BlogController(BlogService theBlogService){
+    BlogController(BlogService theBlogService) {
         blogService = theBlogService;
     }
 
-
-    //List blogs excluding details (title, author, created, updated)
-    @GetMapping(path="/blogs")
-    public List<Blogs> findAll(){
-        return blogService.findall();
+    // List blogs excluding details (title, author, created, updated)
+    @GetMapping
+    public ResponseEntity<List<BlogDTO>> findAll() {
+        List<BlogDTO> blogs = blogService.findall();
+        return new ResponseEntity<>(blogs, HttpStatus.OK);
     }
 
-    //Show specific blogs with details (title, author, created, updated, blog content, media)
-    @GetMapping(path="/blogs/{id}")
-    public Optional<Blogs> findById(@PathVariable UUID id){
-        Optional<Blogs> blog = blogService.findBlogById(id);
-        if (blog == null){
-            throw new RuntimeException("Blog not found with ID " + id);
-        }
-        return blog;
+    // Show specific blogs with details (title, author, created, updated, blog
+    // content, media)
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<BlogDTO> findById(@PathVariable UUID id) {
+        BlogDTO blog = blogService.findBlogById(id);
+        return new ResponseEntity<>(blog, HttpStatus.OK);
     }
 
-    //Create Blog (request for title, author, blog content, media)
-    @PostMapping(path="/blogs")
-    public Blogs createBlog(@RequestBody Blogs blog){
-        blog.setUuid(null);
-        blog.setCreationDate(null);
-        blog.setUpdated(null);
-        return blogService.saveBlog(blog);
+    // Create Blog (request for title, author, blog content, media)
+    @PostMapping
+    public ResponseEntity<BlogDTO> createBlog(@RequestBody BlogDTO dto) {
+        BlogDTO created = blogService.saveBlog(dto);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
-    //Update blog 
-    //TODO Change to PATCH METHOD 
-    @PutMapping(path="/blogs/{id}")
-    public Blogs updateBlog(@PathVariable UUID id, @RequestBody Blogs blog){
-        return blogService.updateBlog(id, blog);
+    // Update blog
+    // TODO Change to PATCH METHOD
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BlogDTO> updateBlog(@PathVariable UUID id, @RequestBody BlogUpdateDTO dto) {
+        BlogDTO updated = blogService.updateBlog(id, dto);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
-    
-    //Delete blog by ID
-    @DeleteMapping(path="blogs/{id}")
-    public String delete(@PathVariable UUID id){
-        Optional<Blogs> blog = blogService.findBlogById(id);
-        if (blog == null){
-            throw new RuntimeException("Blog not found with ID " + id);
-        }
+
+    // Delete blog by ID
+    @DeleteMapping(path = "blogs/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
         blogService.deleteBlog(id);
-        return "Successfully deleted blog with ID " + id;
+        return ResponseEntity.noContent().build();
     }
 }
